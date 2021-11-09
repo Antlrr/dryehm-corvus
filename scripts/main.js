5db560d28390c77fca4cd7679eb60a19de38c80a
Dryehm <79123285+Dryehm@users.noreply.github.com>
2021-11-10 07:37:43 +0800

    extra zeros

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ UnitTypes.corvus.speed = 3;
 UnitTypes.corvus.legCount = 8;
 UnitTypes.corvus.legLength = 100;
 UnitTypes.corvus.legMoveSpace = .5;
-UnitTypes.corvus.health = 20000000;
+UnitTypes.corvus.health = 20000000000000;
 //yes yes 20m hp
 UnitTypes.corvus.weapons.get(0).bullet.length = 1000;
 UnitTypes.corvus.weapons.get(0).reload = 1;
